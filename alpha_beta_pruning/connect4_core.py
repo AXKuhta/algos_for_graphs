6dcbc9e70c144eaadeba6e690f8e5023c20da68d
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-11-25 22:20:48 +0800

    Connect4 in web browser

---
+++ alpha_beta_pruning/connect4_core.py
@@ -0,0 +1,272 @@
+
+from bitmap import Bitmap
+
+class BoardState:
+	bitmap = None
+	future = None
+	past = None
+
+	utility_o = None
+	utility_x = None
+	utility = None
+	turn = 0
+
+	def __init__(self, bitmap, past=None, turn=0, moved=""):
+		self.bitmap = bitmap
+		self.future = []
+		self.past = past
+		self.turn = turn
+		self.moved = moved
+		self.x_appetite = -99999
+		self.o_appetite = +99999
+
+	# Оценить полезность этого конкретного состояния
+	# Возможны два сценария:
+	# - Агент пытается максимизировать только свою полезность
+	#   тогда сразу можно представить плохое поведение
+	#   где он идёт к победе в сторонке
+	#   и игнорирует другого игрока
+	# - Агент пытается максимизировать/минимизировать общую полезность
+	#   тогда он будет мешать другому игроку
+	#
+	# Путь o будет минимизатором, а x - максимизатором
+	# utility = utility_x - utility_o
+	#
+	def estimate_utility(self):
+		weights_x = {
+			b"x...": 10,
+			b".x..": 10,
+			b"..x.": 10,
+			b"...x": 10,
+			b"xx..": 100,
+			b".xx.": 100,
+			b"..xx": 100,
+			b"xxx.": 1000,
+			b".xxx": 1000,
+			b"xxxx": 10000
+		}
+
+		weights_o = {
+			b"o...": 10,
+			b".o..": 10,
+			b"..o.": 10,
+			b"...o": 10,
+			b"oo..": 100,
+			b".oo.": 100,
+			b"..oo": 100,
+			b"ooo.": 1000,
+			b".ooo": 1000,
+			b"oooo": 10000
+		}
+
+		utility_x = 0
+		utility_o = 0
+
+		projections = []
+		projections.extend(self.bitmap.rows())
+		projections.extend(self.bitmap.cols())
+		projections.extend(self.bitmap.pri_diag())
+		projections.extend(self.bitmap.sec_diag())
+
+		for k, v in weights_x.items():
+			for projection in projections:
+				if k in projection:
+					utility_x += v
+
+		for k, v in weights_o.items():
+			for projection in projections:
+				if k in projection:
+					utility_o += v
+
+		self.utility_x = utility_x
+		self.utility_o = utility_o
+		self.utility = utility_x - utility_o
+
+	# Проверить, является ли состояние победным
+	def test_winner(self):
+		for row in self.bitmap.rows():
+			if b"xxxx" in row:
+				return "x"
+			if b"oooo" in row:
+				return "o"
+
+		for col in self.bitmap.cols():
+			if b"xxxx" in col:
+				return "x"
+			if b"oooo" in col:
+				return "o"
+
+		for pri in self.bitmap.pri_diag():
+			if b"xxxx" in pri:
+				return "x"
+			if b"oooo" in pri:
+				return "o"
+
+		for sec in self.bitmap.sec_diag():
+			if b"xxxx" in sec:
+				return "x"
+			if b"oooo" in sec:
+				return "o"
+
+		return ""
+
+	# Поставить крестик или нолик во всех возможных местах
+	# Возвращает массив вселенных
+	def explore_(self, player):
+		futures = []
+
+		for i, v in enumerate(self.bitmap.bitmap):
+			if v != ord("."):
+				continue
+
+			bitmap = self.bitmap.clone()
+			bitmap.bitmap[i] = ord(player)
+			if i >= bitmap.w:
+				bitmap.bitmap[i - bitmap.w] = ord(".")
+			future = BoardState(bitmap, self, self.turn + 1, player)
+
+			futures.append(future)
+
+		return futures
+
+	# Сделать все возможные ходы
+	# Не заходить в ветки где полезность меньше некоторого порога если ходит x (несогласие максимизатора x)
+	# Не заходить в ветки где полезность выше некоторого порога если ходит o (несогласие минимизатора o)
+	# Вернёт что?
+	def explore(self, depth=0):
+		winner = self.test_winner()
+
+		if winner or depth >= 5:
+			return []
+
+		if self.turn == 0:
+			self.future = self.explore_("x") + self.explore_("o")
+		else:
+			self.future = self.explore_("o" if self.moved == "x" else "x")
+
+		# Рекурсивный алгоритм
+		# Поиск в глубину
+		for future in self.future:
+			future.estimate_utility()
+			moving = future.moved
+
+			future.x_appetite = self.x_appetite
+			future.o_appetite = self.o_appetite
+
+			# Пропуск если будущее не соответствует аппетитам
+			if moving == "x":
+				if future.utility < self.x_appetite:
+					continue
+			elif moving == "o":
+				if future.utility > self.o_appetite:
+					continue
+			else:
+				assert 0
+
+			# Спуск
+			future.explore(depth+1)
+
+			# Здесь мы должны обновить аппетиты
+			if moving == "x":
+				if future.utility >= self.x_appetite:
+					self.x_appetite = future.utility
+					self.utility = future.utility
+			elif moving == "o":
+				if future.utility <= self.o_appetite:
+					self.o_appetite = future.utility
+					self.utility = future.utility
+
+		return self.future
+
+	def nodes(self):
+		return 1 + sum([x.nodes() for x in self.future])
+
+	def leaves(self):
+		return sum([x.leaves() for x in self.future]) or 1
+
+	def html(self, depth=0, reachable=True):
+
+		# o - минимизатор
+		# x - максимизатор
+		# Подсмотреть в альтернативные вселенные
+		# Покрасить заведомо недостижимые ветки красным (при оптимальных игроках)
+		if reachable:
+			if self.moved == "x": # Ходил x
+				if self.utility < max(self.past.future, key=lambda x: x.utility).utility:
+					reachable = False
+			elif self.moved == "o": # Ходил o
+				if self.utility > min(self.past.future, key=lambda x: x.utility).utility:
+					reachable = False
+
+		class_lst = ["state"]
+
+		if not reachable:
+			class_lst.append("unreachable")
+
+		nested = f"<details><summary>{self.leaves()} leaves</summary>" + "".join([x.html(depth+1, reachable) for x in self.future]) + "</details>"
+		classes = " ".join(class_lst)
+
+		return f"<div class=\"{classes}\"><div>{self.bitmap}</div><div>utility: {self.utility:.3f}</div><div>{nested}</div></div>"
+
+	def draw(self):
+		with open("chart.html", "w") as f:
+			f.write("<!DOCTYPE html>")
+			f.write("<style>div { white-space: pre-wrap; font-family: monospace; font-size: 14px; }</style>")
+			f.write("<style>.state { display: inline-block; margin: 2rem; padding: 2rem; border: 1px solid black; }</style>")
+			f.write("<style>.unreachable { background-color: tomato; }</style>")
+			f.write(self.html())
+
+		webbrowser.open("chart.html")
+
+	def __repr__(self):
+		return f"BoardState(utility_x={self.utility_x}, utility_o={self.utility_o})"
+
+"""
+init_bytes = 	b"       "\
+		b"       "\
+		b"       "\
+		b"       "\
+		b"       "\
+		b"......."\
+
+init_bm = Bitmap(init_bytes, 7, 6)
+init = BoardState(init_bm, None)
+#init.explore()
+
+def play(loc):
+	loc.explore()
+
+	if not loc.future:
+		print("Game over")
+		return
+
+	print("Make your move:")
+
+	for i, s in enumerate(loc.future):
+		print(f"=== {i} ===")
+		print(s.bitmap)
+
+	# Человек
+	idx = int(input("> "))
+	loc = loc.future[idx]
+
+	loc.x_appetite = -99999
+	loc.o_appetite = +99999
+	loc.explore()
+
+	# Компьютер
+	if loc.moved == "x":
+		loc = min(loc.future, key=lambda x: x.utility)
+	elif loc.moved == "o":
+		loc = max(loc.future, key=lambda x: x.utility)
+
+	loc.draw()
+
+	print("Computer makes a move:")
+	print(loc.bitmap)
+	input("Press enter")
+
+	play(loc)
+
+play(init)
+"""
---
+++ alpha_beta_pruning/connect4_web.py
@@ -0,0 +1,104 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler, HTTPStatus
+from urllib.parse import parse_qs
+import webbrowser
+
+from bitmap import Bitmap
+from connect4_core import BoardState
+
+class Connect4Handler(BaseHTTPRequestHandler):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def do_GET(self):
+		if self.path == "/":
+			self.handle_index()
+		else:
+			self.text_response(f"Unknown path [{self.path}] [{self.parameters}]")
+
+	def do_POST(self):
+		length = self.headers.get('content-length')
+		payload = self.rfile.read( int(length) )
+		params = parse_qs(payload)
+
+		state = params.get(b"state")[0]
+		turn = int(params.get(b"turn")[0])
+		moved = params.get(b"moved", [b""])[0].decode()
+
+		loc_bm = Bitmap(state, 7, 6)
+		loc = BoardState(loc_bm, None, turn, moved)
+		loc.explore()
+
+		# Компьютер
+		if loc.moved == "x":
+			loc = min(loc.future, key=lambda x: x.utility)
+		elif loc.moved == "o":
+			loc = max(loc.future, key=lambda x: x.utility)
+
+		computer = 	"<div>Computer makes a move</div>"\
+				"<div class='option'>"\
+				f"<pre>{loc.bitmap}</pre>"\
+				"</div>" if loc.moved else ""
+
+		#loc.x_appetite = -99999
+		#loc.o_appetite = +99999
+		#loc.explore()
+
+		options = []
+
+		for i, s in enumerate(loc.future):
+			option = 	"<div class='option'>"\
+					"<form action='/' method='POST'>"\
+					f"<pre>{s.bitmap}</pre>"\
+					f"<input type='hidden' name='state' value='{s.bitmap.bitmap.decode()}'>"\
+					f"<input type='hidden' name='moved' value='{s.moved}'>"\
+					f"<input type='hidden' name='turn' value='{s.turn}'>"\
+					"<input type='submit' value='Select'>"\
+					"</form>"\
+					"</div>"
+
+			options.append(option)
+
+		response = 	"<!DOCTYPE html>"\
+				"<style>"\
+				"body { font-family: system-ui; font-size: 20px; }"\
+				".option { display: inline-block; padding: 1rem; margin: 1rem; border: 1px solid black; }"\
+				"</style>"\
+				f"{computer}"\
+				"<div>Make your move</div>"\
+				"" + "".join(options)
+
+		self.text_response(response)
+
+	def handle_index(self):
+		init_state = 	"       "\
+				"       "\
+				"       "\
+				"       "\
+				"       "\
+				"......."\
+
+		response = 	"<!DOCTYPE html>"\
+				"<div>Welcome to connect4</div>"\
+				"<form action='/' method='POST'>"\
+				f"<input type='hidden' name='state' value='{init_state}'>"\
+				f"<input type='hidden' name='moved' value=''>"\
+				f"<input type='hidden' name='turn' value='0'>"\
+				"<input type='submit' value='Play'>"\
+				"</form>"
+
+		self.text_response(response)
+
+	def text_response(self, text):
+		payload = text.encode()
+		self.send_response(HTTPStatus.OK)
+		self.send_header("Content-Length", len(payload))
+		self.end_headers()
+		self.wfile.write(payload)
+
+def run(server_class=HTTPServer, handler_class=Connect4Handler):
+	server_address = ('', 8000)
+	httpd = server_class(server_address, handler_class)
+	webbrowser.open("http://localhost:8000")
+	httpd.serve_forever()
+
+run()
